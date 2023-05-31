d3c387b7cf9f39a923cc199ba8cf22356d148d1c
gladythomas <gladychaprath1999@gmail.com>
2023-05-31 09:01:31 +0530

    Dynamic Adding data to firebase from addmovie component and loader has been updated.

--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -35,6 +35,7 @@ function AddMovie() {
             })
         }
         setLoading(false);
+        
     }
     return (
         <>
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,57 +1,38 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { TailSpin } from 'react-loader-spinner';
 import ReactStars from "react-stars";
-
+import {getDocs} from 'firebase/firestore';
+import { moviesRef } from './firebase/firebase';
 function Cards() {
-    const [data, setData] = useState([
-        {
-            name: " Avengers Infinity War",
-            year: "2018",
-            rating: 5,
-            img: "https://i.etsystatic.com/13367669/r/il/8adffe/1506815473/il_570xN.1506815473_lb94.jpg"
-        },
-        {
-            name: " Avengers Infinity War",
-            year: "2018",
-            rating: 3.5,
-            img: "https://i.etsystatic.com/13367669/r/il/8adffe/1506815473/il_570xN.1506815473_lb94.jpg"
-        },
-        {
-            name: " Avengers Infinity War",
-            year: "2018",
-            rating: 1,
-            img: "https://i.etsystatic.com/13367669/r/il/8adffe/1506815473/il_570xN.1506815473_lb94.jpg"
-        },
-        {
-            name: " Avengers Infinity War",
-            year: "2018",
-            rating: 4,
-            img: "https://i.etsystatic.com/13367669/r/il/8adffe/1506815473/il_570xN.1506815473_lb94.jpg"
-        },
-        {
-            name: " Avengers Infinity War",
-            year: "2018",
-            rating: 5,
-            img: "https://i.etsystatic.com/13367669/r/il/8adffe/1506815473/il_570xN.1506815473_lb94.jpg"
-        },
-        {
-            name: " Avengers Infinity War",
-            year: "2018",
-            rating: 5,
-            img: "https://i.etsystatic.com/13367669/r/il/8adffe/1506815473/il_570xN.1506815473_lb94.jpg"
-        },
-    ]);
+    const [data, setData] = useState([]);
+    const [loading,setLoading]=useState(false);
+
+    useEffect(()=>{
+    async function getData(){
+        setLoading(true);
+        const _data=await getDocs(moviesRef);
+       _data.forEach((doc)=>{
+        setData((prv)=>[...prv,doc.data()])
+       })
+
+        setLoading(false);
+    }
+    getData();
+    },[])
     return (
         <div className='flex flex-wrap justify-between p-3 mt-2'>
+        
             {
+            loading ? <TailSpin height={40} color=''white/> : 
                 data.map((e, i) => {
                     return (
                         <div key={i} className='card  font-medium shadow-lg p-2 hover:-translate-y-3 cursor-pointer mt-6 transition-all duration-500'>
-                            <img className='h-72' src={e.img} alt="" />
+                            <img className='h-72' src={e.image} alt="" />
                             <h1>
                                 <span className='text-green-500'>
                                     Name:
                                 </span>
-                                {e.name}
+                                {e.title}
                             </h1>
                             <h1 className='flex items-center'>
                                 <span className='text-green-500 mr-1'>
@@ -61,7 +42,7 @@ function Cards() {
                                 <ReactStars
                                     size={20}
                                     half={true}
-                                    value={e.rating}
+                                    value={5}
                                     edit={false}
 
                                 />
@@ -73,9 +54,9 @@ function Cards() {
                                 {e.year}
                             </h1>
                         </div>
-                    )
+                    );
                 })}
-
+        
 
         </div>
     )
