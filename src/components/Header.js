368a2f900d092670dedb7adda17303ba9cd294a4
gladythomas <gladychaprath1999@gmail.com>
2023-05-31 10:58:58 +0530

    Changed The title of the Application.

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <div className='text-3xl flex justify-between items-center text-red-500 font-bold p-3 border-b-2 border-gray-500'>
-     <span>Filmy <span className='text-white'>Verse</span></span>
+     <span>Movies <span className='text-white'>Revue</span></span>
     <Link to={"/addmovie"}> <h1 
      className='text-lg text-white flex items-center cursor-pointer'>
      <AddCircleOutlineIcon className='mr-1' color='success'/>
